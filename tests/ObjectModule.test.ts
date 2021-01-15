f41b2126f3605e8d5b43554f22955f079f6f24b1
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-01-15 16:18:28 +0900

    fix: test value

--- tests/ObjectModule.test.ts
+++ tests/ObjectModule.test.ts
@@ -1,15 +1,15 @@
 import { ObjectModule } from "@/modules/ObjectModule";
 
 const user = {
-  lastName: 'tachizaki',
-  firstName: 'koichi',
+  lastName: 'hanetsuki',
+  firstName: 'touka',
   berthDay: '1995-04-10'
 }
 
 describe('Test the ObjectModule.', () => {
   test('Test the pick method response to select the firstName and lastName from user.', () => {
     const resp = ObjectModule.pick(user, 'firstName', 'lastName');
-    expect(resp).toEqual({ lastName: 'tachizaki', firstName: 'koichi' });
+    expect(resp).toEqual({ lastName: 'hanetsuki', firstName: 'touka' });
   });
 
   test('Test the omit method response to exclude the firstName and lastName from user.', () => {
