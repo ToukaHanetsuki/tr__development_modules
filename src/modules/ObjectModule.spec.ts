f8846165c2299229a4d4a6724b7b616d677eabf4
touka.hanetsuki <touka.hanetuki@gmail.com>
2021-01-18 16:44:04 +0900

    [add] test ObjectModule.spec.ts

--- src/modules/ObjectModule.spec.ts
+++ src/modules/ObjectModule.spec.ts
@@ -3,10 +3,16 @@ import { ObjectModule } from './ObjectModule';
 const user = {
   lastName: 'hanetsuki',
   firstName: 'touka',
-  berthDay: '1995-04-10'
+  berthDay: '1995-04-10',
+  age: 25
 };
 
 describe('Test the ObjectModule.', () => {
+  test('Test the accessByBracket method response to select the firstName from user.', () => {
+    const resp = ObjectModule.accessByBracket(user, 'firstName');
+    expect(resp).toBe('touka');
+  });
+
   test('Test the pick method response to select the firstName and lastName from user.', () => {
     const resp = ObjectModule.pick(user, 'firstName', 'lastName');
     expect(resp).toEqual({ lastName: 'hanetsuki', firstName: 'touka' });
@@ -14,6 +20,6 @@ describe('Test the ObjectModule.', () => {
 
   test('Test the omit method response to exclude the firstName and lastName from user.', () => {
     const resp = ObjectModule.omit(user, 'firstName', 'lastName');
-    expect(resp).toEqual({ berthDay: '1995-04-10' });
+    expect(resp).toEqual({ berthDay: '1995-04-10', age: 25 });
   });
 })
